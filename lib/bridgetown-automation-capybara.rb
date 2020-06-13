03bb5a06b417fea22539cc9653e7615792113952
paramagicdev <konnor5456@gmail.com>
2020-06-13 11:59:37 -0400

    remove io_test_helpers

--- lib/bridgetown-automation-capybara.rb
+++ lib/bridgetown-automation-capybara.rb
@@ -2,7 +2,6 @@
 
 require_relative 'bridgetown-automation-capybara/configuration'
 require_relative 'bridgetown-automation-capybara/utils'
-require_relative 'bridgetown-automation-capybara/io_test_helpers'
 
 module CapybaraAutomation
 end
